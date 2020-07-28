1467648014de68c4b8c68a9beec8ec040a7f61d4
akshaybartakke22 <akshaybartakke0@gmail.com>
2020-07-28 21:53:31 +0530

    App basic architecture is added"

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from './services/user.service';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+  loginForm: FormGroup;
+  constructor(
+    private _formbuilder: FormBuilder,
+    private _serverApi: UserService
+  ) {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.loginForm = this._formbuilder.group({
+      userid: ['', Validators.required],
+      userpassword: ['', Validators.required],
+      rememberme: ['']
+    });
+  }
+
+  authenticateUser() {
+    // if (this.loginForm.invalid) {
+    //   return;
+    // }
+
+    let userdata =  Object();
+    userdata.email = this.loginForm.get('userid').value; // admin
+    userdata.password = this.loginForm.get('userpassword').value; // abcd@123
+    this._serverApi.post('/api/login', userdata).subscribe((item) => {
+      console.log(item);
+    })
+  }
+}
